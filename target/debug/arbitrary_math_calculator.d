68e80dfd840353deef26991c9dad6547e224484f
Red Lattice <114372638+Red-Lattice@users.noreply.github.com>
2023-11-25 20:49:50 -0500

    Delete target directory
    Forgot to add this to my gitignore lol

--- target/debug/arbitrary_math_calculator.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Ian\projects\arb_math_calculator\target\debug\arbitrary_math_calculator.exe: C:\Users\Ian\projects\arb_math_calculator\src\main.rs
--- target/debug/deps/arbitrary_math_calculator.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Ian\projects\arb_math_calculator\target\debug\deps\arbitrary_math_calculator.exe: src\main.rs
-
-C:\Users\Ian\projects\arb_math_calculator\target\debug\deps\arbitrary_math_calculator.d: src\main.rs
-
-src\main.rs:
